467bc9efc72962aff5eccc116a33088e4984908f
taorkon-study <tattey.subscriptions@gmail.com>
2024-07-10 14:36:16 +0300

    feat: exercise lesson3 use assert lib in tests instead of if-tests

---
+++ lessons/lesson3/tests/capitalize.test.js
@@ -0,0 +1,7 @@
+import { strict as assert} from 'node:assert';
+import capitalize from '../src/capitalize.js';
+
+assert.equal(capitalize('hello'),'Hello');
+assert.equal(capitalize(''),'');
+
+console.log('All tests passed');
