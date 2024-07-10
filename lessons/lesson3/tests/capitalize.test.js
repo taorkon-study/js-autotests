7e4b2d349d8d49970ecc4524dc2e08a47748d5dc
taorkon-study <tattey.subscriptions@gmail.com>
2024-07-10 14:37:35 +0300

    style(lesson3): eslint fix

--- lessons/lesson3/tests/capitalize.test.js
+++ lessons/lesson3/tests/capitalize.test.js
@@ -1,7 +1,7 @@
-import { strict as assert} from 'node:assert';
+import { strict as assert } from 'node:assert';
 import capitalize from '../src/capitalize.js';
 
-assert.equal(capitalize('hello'),'Hello');
-assert.equal(capitalize(''),'');
+assert.equal(capitalize('hello'), 'Hello');
+assert.equal(capitalize(''), '');
 
 console.log('All tests passed');
